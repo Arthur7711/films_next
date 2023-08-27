c761018349667bb7c0206021985dd5d498a4d57e
Arthur7711 <newartroll11@gmail.com>
2023-08-27 20:58:02 +0400

    hotfix: wrong api call

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function getTodos() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos", {
-    next: { revalidate: 60 }, // Revalidate every 60 seconds
-  });
-  const data = await res.json();
+// export async function getTodos() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/todos", {
+//     next: { revalidate: 60 }, // Revalidate every 60 seconds
+//   });
+//   const data = await res.json();
 
-  return NextResponse.json(data);
-}
+//   return NextResponse.json(data);
+// }
